625dd1149d3fde060f574419d56f86483fe59e5d
Marcos Choma <marcos.choma@gmail.com>
2018-06-09 11:04:16 -0300

    Criação do projeto

---
+++ CrossCutting/Model/ProductRawData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.Model
+{
+    public class ProductRawData
+    {
+        //DESCRIÇÃO SKU CATEGORIA MARCA    PREÇO DE    PREÇO FINAL    PREÇO PONTO POR DESCONTO    BANDEIRA
+        public string Description { get; set; }
+        public string Sku { get; set; }
+        public string Brand { get; set; }
+        public string CostPriceFrom { get; set; }
+        public string CostPrice { get; set; }
+        public string CostPriceInPoints { get; set; }
+        public string Discount { get; set; }
+        public string Vendor { get; set; }
+        public string Url { get; set; }
+
+    }
+}
---
+++ CrossCutting/Model/ProductRawOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.Model
+{
+    public class ProductRawOutput
+    {
+        public string Description { get; set; }
+        public string Stock { get; set; }
+        public string Sku { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string CostPriceFrom { get; set; }
+        public string CostPrice { get; set; }
+        public string CostPriceInPoints { get; set; }
+        public string CheckPrices { get; set; }
+        public string Discount { get; set; }
+        public string Vendor { get; set; }
+        //public string Url { get; set; }
+    }
+}
